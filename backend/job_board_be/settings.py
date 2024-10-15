a886d84cc1549f64f638c21df6b48a1014ba7702
aqibcs <aqibshahcs@gmail.com>
2024-10-15 15:00:46 +0500

    Configure email settings for password reset

--- backend/job_board_be/settings.py
+++ backend/job_board_be/settings.py
@@ -148,3 +148,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
 ]
+
+# Email settings for Gmail SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
